4950469c08a9cca2d18d65617956f2c15a98253e
Filipe Oliveira <filipe.jx.10@gmail.com>
2024-10-15 07:43:34 -0400

    ajustes

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,15 +1,44 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+// CRUD
+// Creat -> POST -> Criar um recado
+// Read -> GET -> Ler todos os recados
+// Read -> GET -> Ler apenas um recado
+// Update -> PATCH / PUT -> Atualizar um recado
+// Delete -> Delete -> Apagar um recado
+
+//PATCH é utilizado para atualizar dados de um recurso
+//PUT é utilizado para atualizar um recurso inteiro
 
 @Controller('recados')
 export class RecadosController {
-    //encontrar todos os recados
+
     @Get()
     findAll() {
         return 'Essa rota retorna todos os recados'
     }
-    //econtra um recado
+
     @Get(':id')
     findOne(@Param('id') id: string){
         return `Essa rota retorna o recado ${id}`
     }
+
+    @Post()
+    create(@Body() body: any) {
+        return body
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body: any) {
+        return {
+            id,
+            ...body
+        }
+    }
+
+    @Delete(':id')
+    romove(@Param('id') id: string) {
+        return `Essa rota APAGA o ID ${id}`;
+    }
+        
 }
