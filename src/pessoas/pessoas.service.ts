6a0ccdda098ddbe4791c78c18d87bc4a84c3e6d4
Filipe Oliveira <filipe.jx.10@gmail.com>
2024-10-16 13:34:18 -0400

    atualizacoa de crud agora ele atualiza o dados no BD

--- src/pessoas/pessoas.service.ts
+++ src/pessoas/pessoas.service.ts
@@ -1,26 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
 import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pessoa } from './entities/pessoa.entity';
+import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PessoasService {
-  create(createPessoaDto: CreatePessoaDto) {
-    return 'This action adds a new pessoa';
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa> //basicamento isso é o banco de dados
+  ) { }
+
+  async create(createPessoaDto: CreatePessoaDto) {
+    try {
+      const dadosPessoa = {
+        nome: createPessoaDto.nome,
+        passwordHash: createPessoaDto.password,
+        email: createPessoaDto.email
+      }
+
+      const novaPessoa = this.pessoaRepository.create(dadosPessoa)
+      await this.pessoaRepository.save(novaPessoa)
+      return novaPessoa
+    } catch (error) {
+      console.log(error)
+      if (error.code === '23505') { //aqui basicamente to capturando o erro com o codigo dele, e informando que o email ja foi cadastro com um if
+        throw new ConflictException('E-mail já está cadastrado')
+      }
+
+      throw error
+    }
+
   }
 
-  findAll() {
-    return `This action returns all pessoas`;
+  async findAll() {
+    const pessoas = await this.pessoaRepository.find({
+      order: {
+        id: 'desc'
+      },
+    })
+
+    return pessoas;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} pessoa`;
   }
 
-  update(id: number, updatePessoaDto: UpdatePessoaDto) {
-    return `This action updates a #${id} pessoa`;
+  async update(id: number, updatePessoaDto: UpdatePessoaDto) {
+    const dadosPessoa = {
+      nome: updatePessoaDto?.nome,  /// basicamaente esse diacho e a atualizacao do user, sendo opcional e dai ele armazena numa variavel
+      passwordHash: updatePessoaDto?.password,
+    }
+    const pessoa = await this.pessoaRepository.preload({
+      id,
+      ...dadosPessoa
+    })
+
+    if (!pessoa) {
+      throw new NotFoundException('Pessoa não encontrada')
+    }
+
+    return this.pessoaRepository.save(pessoa)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pessoa`;
+  async remove(id: number) {
+    const pessoa = await this.pessoaRepository.findOneBy({
+      id
+    })
+
+    if (!pessoa) {
+      throw new NotFoundException('Pessoa não encontrada')
+    }
+
+    return this.pessoaRepository.remove(pessoa)
+
   }
 }
