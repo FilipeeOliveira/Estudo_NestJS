1c7a2df07f112b65f63102361797073f4962d6f3
Filipe Oliveira <filipe.jx.10@gmail.com>
2024-10-21 13:37:38 -0400

    .env database e validacoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,24 @@ import { RecadosModule } from './recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      //envFilePath: '.env', //se eu quiser sleecionar o local do arquivo eu consigo 
+      //ignoreEnvFile: true, // se eu quiser ignorar os arquivos .env
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.required(),
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_DATABASE: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_AUTOLOADENTITIES: Joi.number().min(0).max(1).default(0),
+        DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0),
+      })
+    }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as 'postgres',
       host: process.env.DATABASE_HOST,
