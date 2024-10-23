fa96b12443624abae93be0056c456517cadaff55
Filipe Oliveira <filipe.jx.10@gmail.com>
2024-10-23 14:12:58 -0400

    save de imagem no localhost

--- src/pessoas/pessoas.controller.ts
+++ src/pessoas/pessoas.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
 
 } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
@@ -15,6 +17,9 @@ import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as path from 'path'
+import * as fs from 'fs/promises'
 
 
 @Controller('pessoas')
@@ -51,5 +56,27 @@ export class PessoasController {
   remove(@Param('id') id: string,
     @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
     return this.pessoasService.remove(+id, tokenPayload);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload-picture')
+  async uploadPicture(
+    @UploadedFile() file: Express.Multer.File,
+    @TokenPayloadParam() tokenPayload: TokenPayloadDto
+  ) {
+    const fileExtension = path.extname(file.originalname).toLowerCase().substring(1)
+    const fileName = `${tokenPayload.sub}.${fileExtension}`
+    const fileFullPath = path.resolve(process.cwd(), 'pictures', fileName)
+
+    await fs.writeFile(fileFullPath, file.buffer)
+
+    return {
+      fieldname: file.fieldname,
+      originalname: file.originalname,
+      mimetype: file.mimetype,
+      buffer: {},
+      size: file.size,
+    };
   }
 }
